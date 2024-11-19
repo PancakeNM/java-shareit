59568da228115f3a6f9e7eda8b6dc33b099ff18d
Pancake <nik.moskovenko@yandex.ru>
2024-11-19 19:47:50 +0300

    add mappings to controller

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,12 +1,48 @@
 package ru.practicum.shareit.item;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.service.ItemService;
+
+import java.util.Collection;
 
 /**
  * TODO Sprint add-controllers.
  */
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/items")
 public class ItemController {
+
+    private final ItemService service;
+
+    @GetMapping("/{item-id}")
+    public ItemDto getItemById(@PathVariable("item-id") Long itemId) {
+        return service.getItemById(itemId);
+    }
+
+    @PostMapping
+    public ItemDto saveNewItem(@RequestBody @Valid ItemDto dto,
+                               @RequestHeader("X-Sharer-User-Id") Long userId) {
+        return service.save(dto, userId);
+    }
+
+    @PatchMapping("/{item-id}")
+    public ItemDto updateItem(@RequestBody ItemDto dto,
+                              @RequestHeader("X-Sharer-User-Id") Long userId,
+                              @PathVariable("item-id") Long itemId) {
+        return service.update(dto, itemId, userId);
+    }
+
+    @GetMapping
+    public Collection<ItemDto> getAllItemsByOwnerId(@RequestHeader("X-Sharer-User-Id") Long userId) {
+        return service.getItemsByUserId(userId);
+    }
+
+    @GetMapping("/search?text={}")
+    public Collection<ItemDto> getItemsBySearchPrompt(@RequestParam String text) {
+        return service.getItemsByCertainSearchPrompt(text);
+    }
 }
