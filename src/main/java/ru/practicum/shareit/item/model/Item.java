4f3c16378ff353b067e15aa14c1552babeaf0ca7
Pancake <nik.moskovenko@yandex.ru>
2024-11-19 14:38:58 +0300

    add fields to Item

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,20 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@Builder(toBuilder = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Item {
+    Long id;
+    Long ownerId;
+    String name;
+    String description;
+    Boolean available;
 }
