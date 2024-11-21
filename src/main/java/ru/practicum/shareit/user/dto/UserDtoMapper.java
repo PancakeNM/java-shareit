654427a357ea30dfd9dcfd1656182ae426ae4acc
Pancake <nik.moskovenko@yandex.ru>
2024-11-21 14:30:38 +0300

    make user-dto mapper component

--- src/main/java/ru/practicum/shareit/user/dto/UserDtoMapper.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDtoMapper.java
@@ -1,7 +1,9 @@
 package ru.practicum.shareit.user.dto;
 
+import org.springframework.stereotype.Component;
 import ru.practicum.shareit.user.model.User;
 
+@Component
 public class UserDtoMapper {
 
     public User mapUserFromDto(UserDto dto) {
