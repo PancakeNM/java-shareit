8ab76e303d06c123c72deb323036065689f2d9f1
Pancake <nik.moskovenko@yandex.ru>
2025-02-03 02:16:59 +0300

    update item dto

--- src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
@@ -5,17 +5,12 @@ import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
-
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class ItemDto {
+public class CommentDto {
     long id;
-    String name;
-    String description;
-    boolean available;
-    LocalDateTime nextBooking;
-    LocalDateTime lastBooking;
-    Collection<CommentDto> comments;
+    String text;
+    String authorName;
+    LocalDateTime created;
 }
