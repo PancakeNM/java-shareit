90ed737072d36465785aa747cdb22ee0412b3433
Pancake <nik.moskovenko@yandex.ru>
2025-02-03 02:14:14 +0300

    update user controller

--- src/main/java/ru/practicum/shareit/user/controller/UserController.java
+++ src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -1,8 +1,13 @@
-package ru.practicum.shareit.user;
+package ru.practicum.shareit.user.controller;
 
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.api.ValidateCreateRequest;
+import ru.practicum.shareit.api.ValidateUpdateRequest;
+import ru.practicum.shareit.user.dto.SavedUserDto;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.service.UserService;
 
@@ -10,30 +15,29 @@ import ru.practicum.shareit.user.service.UserService;
  * TODO Sprint add-controllers.
  */
 @RestController
-@RequiredArgsConstructor
+@AllArgsConstructor
 @RequestMapping(path = "/users")
 public class UserController {
-
     private final UserService service;
 
     @GetMapping("/{user-id}")
     public UserDto getUserById(@PathVariable("user-id") Long userId) {
-        return service.getById(userId);
+        return service.getUser(userId);
     }
 
     @PostMapping
-    public UserDto create(@RequestBody @Valid UserDto dto) {
-        return service.create(dto);
+    public UserDto create(@RequestBody @Validated(ValidateCreateRequest.class)SavedUserDto savedUserDto) {
+        return service.createUser(savedUserDto);
     }
 
     @PatchMapping("/{user-id}")
     public UserDto update(@PathVariable("user-id") Long userId,
-                          @RequestBody UserDto dto) {
-        return service.update(dto, userId);
+                          @RequestBody @Validated(ValidateUpdateRequest.class) SavedUserDto savedUserDto) {
+        return service.updateUser(savedUserDto, userId);
     }
 
     @DeleteMapping("/{user-id}")
     public void delete(@PathVariable("user-id") Long userId) {
-        service.delete(userId);
+        service.deleteUser(userId);
     }
 }
