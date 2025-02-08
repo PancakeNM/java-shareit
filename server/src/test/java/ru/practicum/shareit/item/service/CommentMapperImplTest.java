de0abfcb48d3b9c3ce5445ea130bdeb57567bec6
Pancake <nik.moskovenko@yandex.ru>
2025-02-08 20:02:29 +0300

    fix coverage

---
+++ server/src/test/java/ru/practicum/shareit/item/service/CommentMapperImplTest.java
@@ -0,0 +1,22 @@
+package ru.practicum.shareit.item.service;
+
+import org.junit.jupiter.api.Test;
+import ru.practicum.shareit.item.model.Comment;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class CommentMapperImplTest {
+    @Test
+    void map() {
+        CommentSaveDto commentSaveDto = null;
+        CommentMapperImpl commentMapperImpl = new CommentMapperImpl();
+        assertNull(commentMapperImpl.map(commentSaveDto));
+    }
+
+    @Test
+    void testMap() {
+        Comment comment = null;
+        CommentMapperImpl commentMapperImpl = new CommentMapperImpl();
+        assertNull(commentMapperImpl.map(comment));
+    }
+}
